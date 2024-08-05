64c88ca2c5a1ff8b4d3b04f258e3f64541d59aec
heartnetkung <heartnetkung@gmail.com>
2024-08-05 19:01:58 +0100

    init commit

---
+++ gainz/web/api/monitoring/__init__.py
@@ -0,0 +1,5 @@
+"""API for checking project status."""
+
+from gainz.web.api.monitoring.views import router
+
+__all__ = ["router"]
---
+++ gainz/web/application.py
@@ -0,0 +1,52 @@
+import logging
+from importlib import metadata
+
+import sentry_sdk
+from fastapi import FastAPI
+from fastapi.responses import UJSONResponse
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+from sentry_sdk.integrations.logging import LoggingIntegration
+
+from gainz.settings import settings
+from gainz.web.api.router import api_router
+from gainz.web.lifespan import lifespan_setup
+
+
+def get_app() -> FastAPI:
+    """
+    Get FastAPI application.
+
+    This is the main constructor of an application.
+
+    :return: application.
+    """
+    if settings.sentry_dsn:
+        # Enables sentry integration.
+        sentry_sdk.init(
+            dsn=settings.sentry_dsn,
+            traces_sample_rate=settings.sentry_sample_rate,
+            environment=settings.environment,
+            integrations=[
+                FastApiIntegration(transaction_style="endpoint"),
+                LoggingIntegration(
+                    level=logging.getLevelName(
+                        settings.log_level.value,
+                    ),
+                    event_level=logging.ERROR,
+                ),
+            ],
+        )
+    app = FastAPI(
+        title="gainz",
+        version=metadata.version("gainz"),
+        lifespan=lifespan_setup,
+        docs_url="/api/docs",
+        redoc_url="/api/redoc",
+        openapi_url="/api/openapi.json",
+        default_response_class=UJSONResponse,
+    )
+
+    # Main router for the API.
+    app.include_router(router=api_router, prefix="/api")
+
+    return app
