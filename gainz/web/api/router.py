429f042d828ec3996d8a3405f026788f90d4f464
heartnetkung <heartnetkung@gmail.com>
2024-08-06 17:20:52 +0100

    add auth to all routes

--- gainz/web/api/router.py
+++ gainz/web/api/router.py
@@ -1,8 +1,9 @@
-from fastapi.routing import APIRouter
+from fastapi import APIRouter, Depends
 
+from gainz.services.jwt_auth import decode_auth_header
 from gainz.web.api import auth, monitoring, thread
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(auth.router)
-api_router.include_router(thread.router)
+api_router.include_router(thread.router, dependencies=[Depends(decode_auth_header)])
